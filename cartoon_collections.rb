3bd2ab7c60330d916e72d09a8414c1665bd82022
Brian Li <brian.li96@yahoo.com>
2020-11-08 22:36:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,5 +26,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   if (planeteer_calls && valid_calls).any?
     return planeteer_calls
+  else
+    return nil
   end
 end
